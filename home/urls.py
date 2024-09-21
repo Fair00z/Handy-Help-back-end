e53c4ba1d9cb703ea00b14650d67e52be91e9e66
Fairooz <hiiamfairooz@gmail.com>
2024-09-21 00:40:56 -0700

    working on login

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,12 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-     path('', views.index,name='index'),    
+     path('', views.index,name='index'),
+     path('how-it-works/', views.how_it_works,name='how-it-works'),
+     path('about-us',views.about_us,name='about-us'),
+     path('login/',views.login_page,name='login-page'),
+     path('choice/',views.user_choice,name='user-choice'),
+     path('worker_signup/',views.worker_signup,name='worker-signup'),
+     path('logout/', views.logout_view, name='logout'),
+     
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,49 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login,logout
+from django.contrib.auth.models import User
 # Create your views here.
 
 def index(request):
   return render(request,'index.html')
+def how_it_works(request):
+  return render(request,'how-it-works.html')
+
+def about_us(request):
+  return render(request,'About-us.html')
+def login_page(request):
+  if request.method == 'POST':
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+    user = authenticate(request, username=email, password=password)
+
+    if user is not None:
+      # Log the user in
+      login(request, user)
+      return redirect('index')  # Redirect to a homepage or dashboard
+    else:
+      # Add error if authentication fails
+      return redirect('user-choice')
+  return render(request,'login-page.html')
+def user_choice(request):
+  return render(request,'user-choice.html')
+def worker_signup(request):
+  if request.method == 'POST':
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+    conformpassword = request.POST.get('conformpassword')
+
+    user = User.objects.create(
+        username=email,
+        email=email,
+        password=password
+    )
+    if user is not None:
+      login(request, user)
+      return redirect('index')
+    else:
+      return redirect('worker-home.html')
+    
+  return render(request,'worker-signup.html')
+def logout_view(request):
+  logout(request)
+  return redirect('login-page')
